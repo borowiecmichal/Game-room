5fc5287edc26f0b2f9341cfc6eefcfdf642580a2
Michal Borowiec <michalborowiec@op.pl>
2021-01-22 13:45:44 +0100

    mod1 choses available

--- game_room_mod1.py
+++ game_room_mod1.py
@@ -7,13 +7,28 @@ def throw(cube_walls=6):
 
 score_user = 0
 score_comp = 0
+available_dices = (3, 4, 6, 8, 10, 12, 20, 100)
 while score_comp < 2001 and score_user < 2001:
     turn_user = 0
     turn_comp = 0
 
-    input('Press enter to roll the dice')
-    turn_user += throw()
-    turn_user += throw()
+    try:
+        size_1 = input('What kind of dice you want to throw first: ').split('D')
+        size_2 = input('What kind of dice you want to throw second: ').split('D')
+
+        if (size_1[0] == '' and len(size_1) == 2 and int(size_1[1]) in available_dices) and \
+                (size_2[0] == '' and len(size_2) == 2 and int(size_2[1]) in available_dices):
+            size_1 = int(size_1[1])
+            size_2 = int(size_2[1])
+            turn_user += throw(size_1)
+            turn_user += throw(size_2)
+        else:
+            print('Wrong type of dices!')
+            continue
+    except:
+        print('Wrong type of dices!')
+        continue
+
     if (turn_user == 7):
         score_user = score_user // 7
     elif (turn_user == 11):
